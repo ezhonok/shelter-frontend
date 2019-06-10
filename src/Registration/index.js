07d7c74fe043bd39bc4e707a43f1e3dc616bee5d
Svetlana Polyakova <juchini@Svetlanas-MacBook-Air.local>
2019-06-10 00:22:18 -0500

    route change

--- src/Registration/index.js
+++ src/Registration/index.js
@@ -34,7 +34,7 @@ handleSubmit = async (e) => {
 		const parsedResponse = await regResponse.json()
 
 		if (parsedResponse.data === 'registration successful'){
-			this.props.history.push('/logs')
+			this.props.history.push('/home')
 		}
 
 
